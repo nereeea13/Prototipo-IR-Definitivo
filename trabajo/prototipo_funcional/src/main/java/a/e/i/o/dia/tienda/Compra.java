e6eaa365e151ad6b932f83635bbf862fe79b1293
laucss <laucubsan@alum.us.es>
2025-12-03 23:04:07 +0100

    probando ver si va

--- trabajo/prototipo_funcional/src/main/java/a/e/i/o/dia/tienda/Compra.java
+++ trabajo/prototipo_funcional/src/main/java/a/e/i/o/dia/tienda/Compra.java
@@ -1,8 +1,10 @@
 package a.e.i.o.dia.tienda;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import a.e.i.o.dia.model.BaseEntity;
+import jakarta.persistence.OneToMany;
 
 
 
@@ -10,6 +12,9 @@ public class Compra extends BaseEntity {
 
     private LocalDateTime fechaHora; 
     private Double precioTotal; 
+
+    @OneToMany
+    private List<Producto> productos;
 
     
 }
